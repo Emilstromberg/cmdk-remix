cacdfea9aaefc1a05665b81357808fd9337327d6
Emilstromberg <Emil10stromberg@gmail.com>
2023-10-04 17:35:54 +0200

    Fix bug with backspacing (pacocoursey#176)

--- cmdk/src/index.tsx
+++ cmdk/src/index.tsx
@@ -1195,8 +1195,11 @@ function useValue(
           return part.trim().toLowerCase()
         }
 
-        if (typeof part === 'object' && 'current' in part && part.current) {
-          return part.current.textContent?.trim().toLowerCase()
+        if (typeof part === 'object' && 'current' in part) {
+          if (part.current) {
+            return part.current.textContent?.trim().toLowerCase()
+          }
+          return valueRef.current
         }
       }
     })()
